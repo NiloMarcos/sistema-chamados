3603011fb25d9bce76ab8e52b8a01c4aaa64c9fe
Nilo Freitas <nilo.n2m15@gmail.com>
2022-04-27 10:28:40 -0300

    screen signIn styled is fineshed

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,9 +1,48 @@
+import React, { useState } from "react";
+import { Link } from 'react-router-dom';
+import './signin.css';
+
+import Logo from '../../assets/logo.png';
 
 function SignIn() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    
+    alert('Please enter your');
+  }
+
   return (
-    <div>
-      <p>Pagina de login</p>
-    </div>
+      <div className="container-center">
+        <div className="login">
+          <div className="container-logo">
+            <img src={Logo} alt="Logo login" />
+          </div>
+
+          <form onSubmit={handleSubmit}>
+            <h1>Entrar</h1>
+            <input 
+              type="text" 
+              placeholder="email@email.com" 
+              value={email} 
+              onChange={(e) => setEmail(e.target.value)} 
+            />
+            
+            <input 
+              type="password" 
+              placeholder="*********" 
+              value={password} 
+              onChange={(e) => setPassword(e.target.value)} 
+            />
+            
+            <button type="submit">Acessar</button>
+          </form>
+
+          <Link to="/register">Cadastrar-se</Link>
+        </div>
+      </div>
   );
 }
 
