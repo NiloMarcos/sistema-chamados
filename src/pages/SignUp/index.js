08ecc41966a71de0e53897012d146dcba0fa8743
Nilo Freitas <nilo.n2m15@gmail.com>
2022-04-27 10:35:44 -0300

    screen signUp styled is fineshed

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,9 +1,55 @@
+import React, { useState } from "react";
+import { Link } from 'react-router-dom';
+
+import Logo from '../../assets/logo.png';
 
 function SignUp() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [name, setName] = useState('');
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    
+    alert('Please enter your');
+  }
+
   return (
-    <div>
-      <p>Pagina de cadastro</p>
-    </div>
+      <div className="container-center">
+        <div className="login">
+          <div className="container-logo">
+            <img src={Logo} alt="Logo login" />
+          </div>
+
+          <form onSubmit={handleSubmit}>
+            <h1>Cadastrar-se</h1>
+            <input 
+              type="text" 
+              placeholder="Ex: Michel Oliveira" 
+              value={name} 
+              onChange={(e) => setName(e.target.value)} 
+            />
+
+            <input 
+              type="text" 
+              placeholder="email@email.com" 
+              value={email} 
+              onChange={(e) => setEmail(e.target.value)} 
+            />
+            
+            <input 
+              type="password" 
+              placeholder="*********" 
+              value={password} 
+              onChange={(e) => setPassword(e.target.value)} 
+            />
+            
+            <button type="submit">Acessar</button>
+          </form>
+
+          <Link to="/">Já tenho uma conta</Link>
+        </div>
+      </div>
   );
 }
 
